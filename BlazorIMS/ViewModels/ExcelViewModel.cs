b43ae4968f7ed215acf9f6381441365fa88f0f23
kourosh <kourosh23@hotmail.com>
2023-05-24 08:26:35 +0200

    new excel

--- BlazorIMS/ViewModels/ExcelViewModel.cs
+++ BlazorIMS/ViewModels/ExcelViewModel.cs
@@ -3,14 +3,12 @@
     public class ExcelViewModel
     {
         public string? Name { get; set; } = string.Empty;
-        public string? Supplier { get; set; } = string.Empty;
-        public string? Desc { get; set; } = string.Empty;
         public string? Quantity { get; set; } = string.Empty;
-        public string? StoragePlace { get; set; } = string.Empty;
+        public string? Supplier { get; set; } = string.Empty;
+        public string? Category { get; set; } = string.Empty;
+        public string? Location { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
         public string? Comment { get; set; } = string.Empty;
-        public string? CheckedOutQuantity { get; set; } = string.Empty;
-        public string? CheckedOutByEmail { get; set; } = string.Empty;
-        public string? CheckedOutForProject { get; set; } = string.Empty;
-        public string? CheckedOutDate { get; set; } = string.Empty;
+        public string? DataSheet { get; set; } = string.Empty;
     }
 }
